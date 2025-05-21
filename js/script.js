e6ce674229f22829d4641ffcaf5d183449719dc7
David-EsDiaz <71625007+David-EsDiaz@users.noreply.github.com>
2025-05-21 11:29:43 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://44.203.63.226/api/devices";
+const API_URL = "http://100.27.40.201/api/devices";
 const tableBody = document.getElementById("deviceTableBody");
 const statusActual = document.getElementById("statusActual");
 
